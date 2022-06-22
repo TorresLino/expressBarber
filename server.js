6efc68f7f01b365fd868c1205e0d89990726e424
TorresLino <arthur.torres.de.lino@gmail.com>
2022-06-21 23:18:53 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./app.js');
+const port = normalizePort(process.env.PORT || '8000');
+
+function normalizePort(val){
+    const port = parseInt(val);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;
+}
+
+app.listen(port, function(){
+    console.log(`App listening on port ${port}`);
+})
