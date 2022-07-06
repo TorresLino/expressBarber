25287c1ddc78a605f670cb14f71f6e159c89b7f7
TorresLino <arthur.torres.de.lino@gmail.com>
2022-07-05 20:41:13 -0600

    book date

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+const services = [ //development and debug
+{name: 'Normal Cut', info: 'Duration: 30 minutes', price: '$35', path: '/cut'},
+{name: 'Machine Cut', info: 'Duration: 30 minutes', price: '$20', path: '/machine'},
+{name: 'Beard', info: 'Duration: 30 minutes', price: '$25', path: '/beard'},
+{name: 'Cut + Beard', info: 'Duration: 60 minutes', price: '$50', path: '/shave-and-a-haircut'}
+];
+
 router.get('/', function(req, res, next){
-    req.ejs['pageName'] = "Book"
-    res.render('index', req.ejs);
+    req.ejs['pageName'] = "Book";
+    req.ejs['services'] = services;
+    
+    res.render('book-services', req.ejs);
+});
+
+router.get('/:service', function(req, res, next){
+    if(!services.map((s)=>{return s['path'].substring(1)}).includes(req.params['service']))
+        next();
+    else{
+        req.ejs['pageName'] = "Book";
+        req.ejs['service'] = req.params['service'];
+        res.render('book-date', req.ejs);
+    }
+});
+
+router.get('/:service/:date', function(req, res, next){
+    if(!services.map((s)=>{return s['path'].substring(1)}).includes(req.params['service']))
+        next();
+    else{
+        req.ejs['pageName'] = "Book";
+        req.ejs['service'] = req.params['service'];
+        res.render('book-time', req.ejs);
+    }
 });
 
 module.exports = router;
