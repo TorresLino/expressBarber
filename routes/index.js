5d4e7c7c7c09032b1b0f01cc7fddacc38f42b0f2
TorresLino <arthur.torres.de.lino@gmail.com>
2022-09-05 22:06:00 -0600

    dynamic services in index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 import express from "express";
 const router = express.Router();
+import axios from "axios";
 
 router.get('/', function(req, res, next){
-    req.ejs['pageName'] = "Home"
-    res.render('index', req.ejs);
+    axios.get('http://localhost:8000/api/service')
+        .then(apiRes => {
+            var displayServices = []
+            for(var s in apiRes.data){
+                var service = apiRes.data[s];
+                displayServices.push({
+                    name: service.name,
+                    price: '$ ' + service.price
+                })
+            }
+
+            req.ejs['services'] = displayServices;
+            req.ejs['pageName'] = "Home"
+        
+            res.render('index', req.ejs);
+        })  
 });
+
+router.get('/list-bookings', function(req, res, next){
+    console.log('kansado')
+    res.status(200).send('funcionou')
+})
+
 export default router;
